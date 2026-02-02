0599b7f0e4b7cf898c0b884e58e8835dce1eca79
Abok90 <136308423+Abok90@users.noreply.github.com>
2026-02-02 15:07:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def clean_currency(val):
 def load_data():
     # أسماء الملفات كما رفعتها أنت
     files = {
-        "expenses": "New Microsoft Excel Worksheet.xlsx - Expenses.csv",
-        "income": "New Microsoft Excel Worksheet.xlsx - Income.csv"
+        "expenses": "expenses.csv",
+        "income": "income.csv"
     }
     
     try:
@@ -134,3 +134,4 @@ with st.expander("📄 اضغط هنا لعرض جدول تفاصيل المصا
 
 with st.expander("📄 اضغط هنا لعرض جدول تفاصيل التحصيلات"):
     st.dataframe(df_inc_filtered[['التاريخ', 'شركة الشحن', 'المبلغ المحصل (جم)']], use_container_width=True)
+
